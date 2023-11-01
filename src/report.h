ac078f3f5f9a6db438fa0a7e0771526d2393e84b
Daisuke Nako <t20cs033@yamanashi.ac.jp>
2023-11-01 14:28:04 +0900

    to make environment for research

--- src/report.h
+++ src/report.h
@@ -15,7 +15,8 @@ struct kissat;
 
 void kissat_report (struct kissat *, bool verbose, char type);
 
-#define REPORT(LEVEL, TYPE) kissat_report (solver, (LEVEL), (TYPE))
+#define REPORT(LEVEL, TYPE)  \
+  kissat_report(solver, (LEVEL), (TYPE))
 
 #endif
 
